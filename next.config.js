4e6fc1f10799341424ef3eeb0060c14662f84272
Jashaul <djashaul@gmail.com>
2023-03-15 17:57:06 -0400

    configs

--- next.config.js
+++ next.config.js
@@ -1,15 +1,22 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
-    formats: ['image/avif', 'image/webp'],
-    // Twitter Profile Picture
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'pbs.twimg.com',
-        pathname: '/**',
-      },
-    ],
+    loader: 'imgix',
+    path: 'the "domain" of your Imigix source',
   },
   experimental: {
     appDir: true,
