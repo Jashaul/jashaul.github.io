096b023a6e2992c31bc7a8f3d9de1a40feb68323
Jashaul <djashaul@gmail.com>
2023-03-24 10:41:03 -0400

    content update

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -16,11 +16,9 @@ export default function ExperiencePage() {
           <div className="h-[0.2em] bg-neutral-50 dark:bg-neutral-800 mx-2" /> 
           <p className="font-mono text-sm text-neutral-500 tracking-tighter">May 2022 - Aug 2022</p> 
         </div>
-        {/* <p>This is a test</p> */}
         <ul>
-          <li>Accumulated real world data and performed ETL operations to visualize 8 data sources based on wearables.</li>
-          <li>Built dynamic data sources to mimic live data, protecting user data and allowing 3rd party app development.</li>
-          <li>Designed 10 modules with NodeJs and contributed to open source NPM package.</li>
+          <li>Developed ETL pipelines to ingest, transform data from 8+ applications based on wearables such as Garmin, Fitbit, and Oura via. REST APIs using python.</li>
+          <li>Built a statistical model to generate synthetic data by determining the best-fit distribution of the transformed real data from wearable applications, with about 85% accuracy.</li>
         </ul>
         <h2 id="madras-global-1">
           <a className="anchor" href="#madras-global-1">
@@ -36,9 +34,9 @@ export default function ExperiencePage() {
           <p className="font-mono text-sm text-neutral-500 tracking-tighter">Jun 2019 - Jul 2021</p> 
         </div>
         <ul>
-          <li>Led a team of 3 with Agile to build rich media application, allowing viewing metrics, publishing, scheduling, and modifying ads via Google and Facebook ads.</li>
-          <li>Collaborated with a cross-functional team to automate +10 workflow services for internal and external clients. Worked with internal stakeholders and customers to incorporate feedback into application.</li>
-          <li>Proposed and spearheaded a serverless cloud automation pipeline to extract files and metadata of zip files uploaded to AWS S3, increasing client side rendering and efficiency by 70%.</li>
+          <li>Led a team of 3 with Agile to build rich media application, allowing viewing metrics, publishing, scheduling, and modifying ads posted on Google and Facebook ads sites.</li>
+          <li>Collaborated with cross functional teams to automate +10 workflow services for internal and external clients, and incorporated feedback from stakeholders and customers into application.</li>
+          <li>Proposed and spearheaded a serverless cloud automation pipeline to extract files and metadata of zip files uploaded to AWS S3 using python, increasing client side rendering and efficiency by 70%.</li>
           <li>Implemented full-stack process creating restful APIs with Django framework at backend and ReactJs at frontend for online proofing tool, allowing users to request changes to media assets.</li>
           <li>Explored and modified the Django framework following MTV architecture to run ReactJS at template layer as a single server, reducing resource utilization and deployment time by 30%.</li>
         </ul>
